e3ab718deb2536c2fe0b8e9649c9a80471f9add8
Lis-art <lkesselheim@gmail.com>
2023-05-14 21:19:25 +0200

    finisher move

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,8 @@ let roundCounter = 0;
 let maxRoundNum = document.querySelector("input:checked").value;
 
 
-
+// die Summe der Rundenzahlen wird mit der Maximal Rundenzahl abgeglichen und stoppt nach X Runden
+// mit set Timeout wird die Reset Funktion zeitverzögert aufgerufen, damit das Gesamtergebnis länger gespeichert ist
 function compareRounds(){
 if (roundCounter >= maxRoundNum){
     andTheWinnerIs();
@@ -25,47 +26,52 @@ if (roundCounter >= maxRoundNum){
     }
 };
 
-
+// mit queryAll werden alle Bilde angesteuert und die entsprechenden Funktionen allen Bildern zugeordnet
+// jedes Bild als item - über id wird Name des jeweiligen Bildes ausgegeben
+// Runden Counter wird pro Click 1 hoch gezählt
+// nach User Click wird Funktion choice CPU ausgeführt 
+// battle Winner ermittelt welches Zeichen gewinnt und vergibt entsprechend den Punkt
+// compare rounds gleicht rundenanzahl ab
 document.querySelectorAll("img").forEach((item) => {
     item.addEventListener("click", (e) => {
         e.preventDefault();
-        console.log(roundCounter, maxRoundNum);
-        console.log(item.id);
+        //console.log(roundCounter, maxRoundNum);
+        //console.log(item.id);
         roundCounter++;
         userChoice = item.id;
         cpuRandomChoice();
-        console.log(cpuChoice);
+        //console.log(cpuChoice);
         battleWinner();
         compareRounds();
     });
 });
 
+// Auswahl Runden Radio Btns wird als Zahlwert ausgegeben wird
 document.querySelectorAll("input").forEach((radioBtn) => {
 radioBtn.addEventListener("change", (e) => {
         e.preventDefault();
-        console.log(radioBtn.value); 
+        //console.log(radioBtn.value); 
         maxRoundNum = Number(radioBtn.value);
-        console.log(maxRoundNum);
+        //console.log(maxRoundNum);
     });
 });
 
-
+// Function in der eine Zahl von 1 bis drei random gewählt wird + Festlegung welche Zahl welchem Zeichen entspricht
 function cpuRandomChoice () {
-    let randomNumber = Math.floor(Math.random() * 3);
-    
+    let randomNumber = Math.floor(Math.random() * 3);  
     if (randomNumber === 0) {
         cpuChoice = rock;
-        console.log("CPU hat Stein gewählt");
+        //console.log("CPU hat Stein gewählt");
     } else if (randomNumber === 1) {
         cpuChoice = paper;
-        console.log("CPU hat Papier gewählt");
+        //console.log("CPU hat Papier gewählt");
     } else {
         cpuChoice = scissors;
-        console.log("CPU hat Schere gewählt");
+        //console.log("CPU hat Schere gewählt");
     }
 };
 
-
+// Var für Punktestand zu Begin auf 0 gesetzt
 let winsCPU = 0;
 let winsUSER = 0;
 
@@ -93,34 +99,30 @@ function battleWinner () {
     outputCpuResult.innerHTML = winsCPU;
 };
 
-// Funktion wer nach Rundenende der Gesamtsieger ist
+// Funktion wer nach Rundenende der Gesamtsieger ist + Ausgabe im HTML
+// zusätzlich wird letztes Ergebnis der Runde überschrieben
 function andTheWinnerIs() {
     if (winsUSER > winsCPU){
-        console.log("USER is WINNER");
+        //console.log("USER is WINNER");
         document.querySelector("#finalResult").innerHTML = "CONGRATULATIONS you beat the CPU";
         document.querySelector("#WhoBeatsWho").innerHTML = "";
     } else if (winsCPU > winsUSER){
-        console.log("CPU is WINNER");
+        //console.log("CPU is WINNER");
         document.querySelector("#finalResult").innerHTML = "UFF maybe next time, you LOOSE";
         document.querySelector("#WhoBeatsWho").innerHTML = "";
     } else {
-        console.log("NO WINNER");
+        //console.log("NO WINNER");
         document.querySelector("#finalResult").innerHTML = "OH HA you're as good as a Machine";
         document.querySelector("#WhoBeatsWho").innerHTML = "";
     }
 };
 
-
 // Reset Button Function
+// alle Werte in output Feldern werden auf 0 gesetzt
+// Texte im HTML werden auf Start Texte zurückgesetzt
 document.querySelector("button").addEventListener("click", (e) => {
     e.preventDefault();
-    winsCPU = 0;
-    winsUSER = 0;
-    roundCounter = 0;
-    document.querySelector(".userResult").innerHTML = 0;
-    document.querySelector(".cpuResult").innerHTML = 0;
-    document.querySelector("#finalResult").innerHTML = "Let's play";
-    document.querySelector("#WhoBeatsWho").innerHTML = "";
+    reset();
 });
 
 function reset() {
