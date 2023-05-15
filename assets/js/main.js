fbf6e7b7cd4708be2e6df018f5c416d534aec124
Lis-art <lkesselheim@gmail.com>
2023-05-15 14:58:34 +0200

    with comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,9 @@ const outputCpuResult = document.querySelector(".cpuResult");
 
 
 // Var Auswahl
-let rock = "rock";
-let paper = "paper";
-let scissors = "scissor";
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissor";
 
 let userChoice;
 let cpuChoice;
@@ -35,12 +35,9 @@ if (roundCounter >= maxRoundNum){
 document.querySelectorAll("img").forEach((item) => {
     item.addEventListener("click", (e) => {
         e.preventDefault();
-        //console.log(roundCounter, maxRoundNum);
-        //console.log(item.id);
         roundCounter++;
         userChoice = item.id;
         cpuRandomChoice();
-        //console.log(cpuChoice);
         battleWinner();
         compareRounds();
     });
@@ -50,9 +47,7 @@ document.querySelectorAll("img").forEach((item) => {
 document.querySelectorAll("input").forEach((radioBtn) => {
 radioBtn.addEventListener("change", (e) => {
         e.preventDefault();
-        //console.log(radioBtn.value); 
         maxRoundNum = Number(radioBtn.value);
-        //console.log(maxRoundNum);
     });
 });
 
@@ -61,13 +56,10 @@ function cpuRandomChoice () {
     let randomNumber = Math.floor(Math.random() * 3);  
     if (randomNumber === 0) {
         cpuChoice = rock;
-        //console.log("CPU hat Stein gewählt");
     } else if (randomNumber === 1) {
         cpuChoice = paper;
-        //console.log("CPU hat Papier gewählt");
     } else {
         cpuChoice = scissors;
-        //console.log("CPU hat Schere gewählt");
     }
 };
 
@@ -82,17 +74,14 @@ function battleWinner () {
     if (userChoice == rock && paper == cpuChoice || 
         userChoice == paper && scissors == cpuChoice || 
         userChoice == scissors && rock == cpuChoice){
-        console.log("CPU Win");
         winsCPU += 1;
         document.querySelector("#WhoBeatsWho").innerHTML = "You are the LOOSER"
     } else if (userChoice == paper && rock == cpuChoice || 
         userChoice == scissors && paper  == cpuChoice ||
         userChoice == rock && scissors == cpuChoice){
-        console.log("USER Win");
         winsUSER += 1;
         document.querySelector("#WhoBeatsWho").innerHTML = "You're the WINNER"
     } else {
-        console.log("DRAW");
         document.querySelector("#WhoBeatsWho").innerHTML = "DRAW"
     }
     outputUserResult.innerHTML = winsUSER;
@@ -103,15 +92,12 @@ function battleWinner () {
 // zusätzlich wird letztes Ergebnis der Runde überschrieben
 function andTheWinnerIs() {
     if (winsUSER > winsCPU){
-        //console.log("USER is WINNER");
         document.querySelector("#finalResult").innerHTML = "CONGRATULATIONS you beat the CPU";
         document.querySelector("#WhoBeatsWho").innerHTML = "";
     } else if (winsCPU > winsUSER){
-        //console.log("CPU is WINNER");
         document.querySelector("#finalResult").innerHTML = "UFF maybe next time, you LOOSE";
         document.querySelector("#WhoBeatsWho").innerHTML = "";
     } else {
-        //console.log("NO WINNER");
         document.querySelector("#finalResult").innerHTML = "OH HA you're as good as a Machine";
         document.querySelector("#WhoBeatsWho").innerHTML = "";
     }
